030fe32cb9e6787bb5ceb1dc8342c35f9a7130e8
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-06-16 16:22:32 -0500

    LAB3.5

---
+++ ConsoleAppLAB3.5/ConsoleAppLAB3.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppLAB3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Fire1 = "bomb";
+
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(Fire1))
+            {
+                isSpam = true;
+                Console.WriteLine("This message is spam. Create a new message");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This message is not spam. This message is accepted");
+            }
+
+
+        }
+    }
+}
