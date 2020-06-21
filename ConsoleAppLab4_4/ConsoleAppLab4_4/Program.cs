5cb97a1ee675e9d6f6dc9eb2c626e4e5a6368381
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-06-21 09:57:15 -0500

    Lab4-4

---
+++ ConsoleAppLab4_4/ConsoleAppLab4_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppLab4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] Blacklist = {"buy", "viagra", "XXX", "free money", "lifetime offer", "send money", "bank account", "nigeria", "online",
+            "pharmacy", "h8te", "meet girls"};
+
+            string message = Console.ReadLine();
+            bool isSpam = false;
+            message = message.ToLower();
+            for (int i = 0; i < Blacklist.Length; i++)
+            {
+                if (message.Contains(Blacklist[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+
+            if (isSpam == true)
+            {
+                Console.WriteLine("The message contains spam. Write another one.");
+            }
+            else
+            {
+                Console.WriteLine("The message is accepted");
+            }
+        }
+    }
+}
