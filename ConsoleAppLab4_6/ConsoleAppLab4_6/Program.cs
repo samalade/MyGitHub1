1cea0dd2cb1bc75e49e78330e659253fd3f8d922
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-06-23 08:20:37 -0500

    LAB4_6

---
+++ ConsoleAppLab4_6/ConsoleAppLab4_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppLab4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] Days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string[] Tasks = new string[7];
+
+
+            for (int i = 0; i < Days.Length; i++)
+            {
+
+                Console.WriteLine("Type in a new Task for " + Days[i]);
+                Tasks[i] = Days[i] + " : " + Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < Days.Length; i++)
+            {
+                Console.WriteLine(Tasks[i]);
+            }
+        }
+    }
+}
