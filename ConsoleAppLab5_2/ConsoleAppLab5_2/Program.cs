597f7bc7162c63effd1b61364454bd884fcaae90
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-06-23 10:23:49 -0500

    LAB5_2

---
+++ ConsoleAppLab5_2/ConsoleAppLab5_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace ConsoleAppLab5_2
+{
+    class Program
+    {
+
+
+        enum Days
+        {
+
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<string> taskList = new List<string>();
+
+            foreach (Days day in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
+
+            }
+
+            foreach(string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
