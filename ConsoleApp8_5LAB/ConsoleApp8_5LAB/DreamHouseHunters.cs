05177bfcc220f24df1b8446d3454bc48a2d158aa
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-07-07 08:30:10 -0500

    trial 8_5

---
+++ ConsoleApp8_5LAB/ConsoleApp8_5LAB/DreamHouseHunters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8_5LAB
+{
+    class DreamHouseHunters: Application
+    {
+        public string JobTitle;
+        public int CurrentAnnualIncome;
+        public int BuyingHouseMonth;
+        public int NumOfBedrooms;
+        public int NumOfBathrooms;
+
+        public DreamHouseHunters(string jobTitle, int currentIncome, int BuyingHouseMonth, int NumOfBedrooms, int NumOfBathrooms,
+            string firstName, string lastName, string dateOfBirth, string address, string city, string state, int zip,
+            string phoneNumber, string email) : base(firstName, lastName, dateOfBirth, address, city, state, zip, phoneNumber, email)
+        {
+            this.JobTitle = jobTitle;
+            this.CurrentAnnualIncome = currentIncome;
+            this.BuyingHouseMonth = BuyingHouseMonth;
+            this.NumOfBedrooms = NumOfBedrooms;
+            this.NumOfBathrooms = NumOfBathrooms;
+        }
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("You've been accepted to the TV show: Dream House Hunters");
+        }
+    
+    }
+}
---
+++ ConsoleApp8_5LAB/ConsoleApp8_5LAB/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp8_5LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DreamHouseHunters hhContestant = new DreamHouseHunters("Full Stack Web Developer", 100000, 6, 3, 2, "Omar", "Smith", "2/4/1975",
+                "111 Bravo Way", "Los Angeles", "California", 90001, "888-233-1234", "omar.smith@hotmail.com");
+
+            ParadiseIsland piContestant = new ParadiseIsland("Female", "Sidney", 2, "Levi", "Washington", "5/4/1994", "211 MLK Rd", "Queens",
+                "New York", 11367, "347-444-1234", "Sidney.wash@gmail.com");
+
+            AboveDeck adContestant = new AboveDeck(26, "American", "Darren", "Lee", "7/12/1964", "311 Kew Gargen Hills", "Flushing",
+            "New York", 11360, "347-333-1234", "D.Lee@gmail.com");
+
+            hhContestant.Submit();
+            hhContestant.Accept();
+
+            piContestant.Submit();
+            piContestant.Accept();
+
+            adContestant.Submit();
+            adContestant.Accept();
+        }
+    }
+}
