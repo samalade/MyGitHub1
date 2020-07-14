f93c202ad64876a47b046bb27a9b812e810a0620
Samuel Aladegbemi <SAMUELALADEGBEMI@GMAIL.COM>
2020-07-14 08:29:03 -0500

    10_4lab

---
+++ ConsoleApp10_4lab/ConsoleApp10_4lab/Interface1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10_4lab
+{
+    interface IAudioPlayer
+    {
+        void Play();
+    }
+}
---
+++ ConsoleApp10_4lab/ConsoleApp10_4lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp10_4lab
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Artist kendrick = new Artist("Kendrick Lamar");
+            kendrick.AddToPlaylist("ADHD");
+            kendrick.AddToPlaylist("Alright");
+            kendrick.AddToPlaylist("Sing About Me");
+            kendrick.AddToPlaylist("Rigamortis");
+
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(kendrick);
+
+            AudioBook howto = new AudioBook("The Achelmist", "Paulo Coelho");
+            player.PlayMedia(howto);
+
+            MovieSoundTrack Inception = new MovieSoundTrack("Inception");
+            Inception.SelectTrack("Opening theme : Time");
+            player.PlayMedia(Inception);
+        }
+    }
+}
+
+
